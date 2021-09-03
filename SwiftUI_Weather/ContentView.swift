c4581178b73be47751cc765b0db19efd122e6997
VBel3235 <Vlad95.0753@gmail.com>
2021-09-03 17:21:11 +1000

    Initial Commit

---
+++ SwiftUI_Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Weather
+//
+//  Created by Владислав Белов on 03.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
